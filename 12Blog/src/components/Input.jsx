40b15f53e91aa2ca105427bfad31332b76807fd2
MayurJirapure11 <mrjirapure@gmail.com>
2025-01-16 18:04:23 +0530

    React Components

---
+++ 12Blog/src/components/Input.jsx
@@ -0,0 +1,30 @@
+import React, { useId } from 'react'
+
+// forwardref : is used when we use the same component at diff places , we need access of its state to give ref for accessing it's state it is used
+const Input = React.forwardRef(function Input({
+    label,
+    type = "text",
+    className = "",
+    ...props
+}, ref){
+    const id = useId()
+    return (
+        <div className='w-full'>
+            {label && <label 
+            className='inline-block mb-1 pl-1' 
+            htmlFor={id}>
+                {label}
+            </label>
+            }
+            <input
+            type={type}
+            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+            ref={ref}
+            {...props}
+            id={id}
+            />
+        </div>
+    )
+})
+
+export default Input
